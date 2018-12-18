ddeba4db354a38a6d5705c07706f39c2e6039f12
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-12-18 15:55:29 -0500

    some css styling and expanded the fetch request.

--- music-master/src/App.js
+++ music-master/src/App.js
@@ -9,15 +9,16 @@ class App extends Component {
     super(props);
     this.state = {
       query: "",
-      artist: null
+      artist: null,
+      tracks: []
     };
   }
 
   search() {
     console.log("this.state", this.state);
     const BASE_URL = "https://api.spotify.com/v1/search?";
-    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-    const ALBUM_URL = "https://api.spotify.com/v1/artists/";
+    let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    let ALBUM_URL = "https://api.spotify.com/v1/artists/";
     const accessToken = myAccessToken;
     var myHeaders = new Headers();
 
@@ -70,8 +71,18 @@ class App extends Component {
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
-        <Profile artist={this.state.artist} />
-        <div className="Gallery">Gallery</div>
+            {
+                this.state.artist !== null
+            ?
+            <div>
+
+             <Profile artist={this.state.artist} />
+                <div className="Gallery">
+                Gallery
+                </div>
+            </div>
+                : <div></div>
+            }
       </div>
     );
   }
