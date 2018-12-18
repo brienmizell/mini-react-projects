dbd7f06409d585953123955c57cfe760367d07ea
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-12-17 22:05:27 -0500

    created a Clock componet, calculated time until algorithm, and added lifecycle methods for updating clock

--- date-countdown/src/App.js
+++ date-countdown/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import Clock from "./Clock";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deadline: "December 25, 2017",
+      deadline: "December 25, 2018",
       newDeadline: ""
     };
   }
@@ -18,12 +19,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-title">Countdown to {this.state.deadline}</div>
-        <div>
-          <div className="Clock-days">14 days</div>
-          <div className="Clock-hours">30 hours</div>
-          <div className="Clock-minutes">15 minutes</div>
-          <div className="Clock-seconds">20 seconds</div>
-        </div>
+        <Clock deadline={this.state.deadline} />
         <input
           placeholder="New Date"
           onChange={event => this.setState({ newDeadline: event.target.value })}
