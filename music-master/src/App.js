a8bd7e501d217569fd28e66d3e3eed20413b70e5
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-12-30 13:01:38 -0500

    tyring to implement auto refreshing of authcode.

--- music-master/src/App.js
+++ music-master/src/App.js
@@ -3,13 +3,22 @@ import "./App.css";
 import { FormGroup, FormControl, InputGroup, Glyphicon } from "react-bootstrap";
 import Profile from "./Profile";
 import Gallery from "./Gallery";
+import { id, secret } from "./secret/spotify-credentials";
 // const express = require('express');
-// const request = require('request');
-// const base64 = require('base-64');
+const request = require("request");
+const base64 = require("base-64");
 
 class App extends Component {
+  AUTHORIZATION_HEADER = "";
+  globalAccessToken = "";
+
   constructor(props) {
     super(props);
+
+    // this.AUTHORIZATION_HEADER = base64.encode(`${id}:${secret}`);
+    this.AUTHORIZATION_HEADER = new Buffer(id + ":" + secret).toString(
+      "base64"
+    );
     this.state = {
       query: "",
       artist: null,
@@ -17,8 +26,53 @@ class App extends Component {
     };
   }
 
+  requestNewToken() {
+    console.log(id, secret);
 
+    // var authOptions = {
+    //   url: "https://accounts.spotify.com/api/token",
+    //   method: "POST",
+    //   headers: {
+    //     Authorization: `Basic ${this.AUTHORIZATION_HEADER}`,
+    //     Cookie: "test"
+    //   },
 
+    // };
+
+    fetch("/", {
+      method: "GET"
+      // headers: {
+      //   Authorization: `Basic ${this.AUTHORIZATION_HEADER}`
+      // },
+      // mode: "cors",
+      // cache: "default",
+      // form: { grant_type: "client_credentials" },
+      // json: true
+    })
+      .then(response => response.json())
+      .then(json => {});
+
+    // request.post(authOptions, function(error, response, body) {
+    //   console.log(JSON.stringify(response));
+    //   console.log(JSON.stringify(error));
+    //   console.log(JSON.stringify(body));
+    // });
+
+    // request(
+    //   {
+    //     url: "https://accounts.spotify.com/api/token",
+    //     method: "POST",
+    //     headers: { Authorization: `Basic ${this.AUTHORIZATION_HEADER}` },
+    //     form: { grant_type: "client_credentials" }
+    //   },
+    //   (error, response, body) => {
+    //     console.log(JSON.stringify(response));
+    //     console.log(JSON.stringify(error));
+    //     console.log(JSON.stringify(body));
+    //     this.globalAccessToken = JSON.parse(body).access_token;
+    //   }
+    // );
+  }
 
   search() {
     console.log("this.state", this.state);
@@ -26,7 +80,7 @@ class App extends Component {
     let FETCH_URL = BASE_URL + "q=" + this.state.query + "&type=artist&limit=1";
     const ALBUM_URL = "https://api.spotify.com/v1/artists/";
     var accessToken =
-      "BQCWCL0KhxsCCfk6gyB8en1Bwa0Fc3OBCq6_1FhiMdk4CzheyfOnefcponCLi47HKv_nFYRgdGzPnMemQWSIMqbEAr9IF9ToM7la7KIOY4YR_0421JukV74kme2_nfZrQyuBzVfY70CVvjbmD8C-FboOUweu-V0xlNl5PQ&refresh_token=AQDFkpAcjcy3Sb4kKzvzOT0d1pwtKdYvSQJJeDpFBdhRYPQOxoSOIq2FrmIC5t3nbabOvDSelK4PSKDM6uJkqZkplFjC-2FIb_0gqW7EtNEXIDErBs3SrHO1Y9vbd1irkORNsQ";
+      "BQAv1OrsRGTBkUR7Wy1Jm_sW0iqzk5S5FV3leTL07DljbU9sWN17xDecpn0jp3gX8UVX2L2Yof61V05x9s73Ne5_ys2AhWZy1CspCFue3zeS2Ntiy47XURt-yGiT_JEnfn3RSbxfIcT1Xns55qg6gf-SZPbw-SZjRPdcS5j4wAX_bAYnjhJobN7sh264VjCkv5u7N_hpBKi7e81op9nUTgaIVIx8rNfmHCEqAOsiWBu2OX_zPXYSRLa-osH3mNohj4fyfqxv_mt8V0XaAWErqg";
 
     fetch(FETCH_URL, {
       method: "GET",
@@ -50,16 +104,14 @@ class App extends Component {
           mode: "cors",
           cache: "default"
         })
-        .then(response => response.json())
-        .then(json => {
-          console.log('artist\'s top tracks:', json);
-          const { tracks } = json;
-          this.setState({tracks});
+          .then(response => response.json())
+          .then(json => {
+            console.log("artist's top tracks:", json);
+            const { tracks } = json;
+            this.setState({ tracks });
           });
       });
   }
-
- 
 
   render() {
     return (
@@ -81,6 +133,9 @@ class App extends Component {
               }}
             />
             <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search" />
+            </InputGroup.Addon>
+            <InputGroup.Addon onClick={() => this.requestNewToken()}>
               <Glyphicon glyph="search" />
             </InputGroup.Addon>
           </InputGroup>
