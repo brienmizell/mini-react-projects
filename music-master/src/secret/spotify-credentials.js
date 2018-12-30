162ec45a09c224c81e00e4e3d025b09b7565f1b8
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-12-30 13:01:38 -0500

    tyring to implement auto ;5Brefreshing of authcode

--- music-master/src/secret/spotify-credentials.js
+++ music-master/src/secret/spotify-credentials.js
@@ -5,4 +5,4 @@ var client_id = "CLIENT_ID"; // Your client id
 var client_secret = "CLIENT_SECRET"; // Your secret
 var redirect_uri = "REDIRECT_URI"; // Your redirect uri
 
-export default spotify - credentials;
+export { id, secret };
