1ad38f73fc4dfbeb9e5bd51de382a6118bec585e
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-03-07 14:09:34 -0500

    Created a Social Networks Component.

---
+++ portfolio/src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from "../assets/email_icon.png";
+import githubIcon from "../assets/github_icon.png";
+import linkedinIcon from "../assets/linkedin_icon.png";
+import twitterIcon from "../assets/twitter_icon.png";
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: "mailto:brienmizell@me.com",
+    image: emailIcon
+  },
+  {
+    id: 2,
+    link: "https://github.com/brienmizell",
+    image: githubIcon
+  },
+  {
+    id: 3,
+    link: "https://www.linkedin.com/in/brienmizell",
+    image: linkedinIcon
+  },
+  {
+    id: 4,
+    link: "https://twitter.com/brienmizell",
+    image: twitterIcon
+  }
+];
+
+export default SOCIAL_PROFILES;
