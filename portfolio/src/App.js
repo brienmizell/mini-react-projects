409a42e8edfdd460453a37a02d6528a6b93d32c5
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-03-06 20:07:17 -0500

    Added displayBio info into state.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,19 +3,27 @@ import React, { Component } from "react";
 class RegularClass {}
 class ComponentClass extends Component {}
 
-const regularClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
-
-console.log("regularClassInstance", regularClassInstance);
-console.log("componentClassInstance", componentClassInstance);
-
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: false };
+  }
   render() {
     return (
       <div>
         <h1>Hello!</h1>
         <p>My name is Brien. I'm a software engineer.</p>
         <p>I'm always looking forward to working on meaningful projects.</p>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in Decatur, GA. I code every day.</p>
+            <p>
+              My favoirte language is JavaScript, and I think React.js is
+              awesome.
+            </p>
+            <p>Besides coding, I also love music, beer, sports and ramen!</p>
+          </div>
+        ) : null}
       </div>
     );
   }
