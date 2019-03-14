ae5b84d4102d45c9f3eabf0b304223a74d9a57b3
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-03-14 17:27:30 -0400

    Added a button to retrieve 10 new jokes when clicked.

--- portfolio/dist/src.e31bb0bc.js
+++ portfolio/dist/src.e31bb0bc.js
@@ -26031,6 +26031,17 @@ function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || func
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
+var Joke = function Joke(_ref) {
+  var _ref$joke = _ref.joke,
+      setup = _ref$joke.setup,
+      punchline = _ref$joke.punchline;
+  return _react.default.createElement("p", {
+    style: {
+      margin: 20
+    }
+  }, setup, " ", _react.default.createElement("em", null, punchline));
+};
+
 var Jokes =
 /*#__PURE__*/
 function (_Component) {
@@ -26050,7 +26061,18 @@ function (_Component) {
     _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(Jokes)).call.apply(_getPrototypeOf2, [this].concat(args)));
 
     _defineProperty(_assertThisInitialized(_this), "state", {
-      joke: {}
+      joke: {},
+      jokes: []
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "fetchJokes", function () {
+      fetch("https://official-joke-api.appspot.com/random_ten").then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        return _this.setState({
+          jokes: json
+        });
+      });
     });
 
     return _this;
@@ -26072,10 +26094,16 @@ function (_Component) {
   }, {
     key: "render",
     value: function render() {
-      var _this$state$joke = this.state.joke,
-          setup = _this$state$joke.setup,
-          punchline = _this$state$joke.punchline;
-      return _react.default.createElement("div", null, _react.default.createElement("h2", null, "Highlighted Joke"), _react.default.createElement("p", null, setup, " ", _react.default.createElement("em", null, punchline)));
+      return _react.default.createElement("div", null, _react.default.createElement("h2", null, "Highlighted Joke"), _react.default.createElement(Joke, {
+        joke: this.state.joke
+      }), _react.default.createElement("hr", null), _react.default.createElement("h3", null, "Want ten new jokes?"), _react.default.createElement("button", {
+        onClick: this.fetchJokes
+      }, "Click me!"), this.state.jokes.map(function (joke) {
+        return _react.default.createElement(Joke, {
+          key: joke.id,
+          joke: joke
+        });
+      }));
     }
   }]);
 
@@ -26293,7 +26321,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59304" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60088" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
