cefc022c15f917ca50b379dae6990550c9efb5c6
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-03-21 14:07:32 -0400

    Added a Search Component and lifted state up in callback props.

--- music-master2.0/src/components/App.js
+++ music-master2.0/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Search from "./Search";
 import Artist from "./Artist";
 import Tracks from "./Tracks";
 
 const API_ADDRESS = "https://spotify-api-wrapper.appspot.com";
 
 class App extends Component {
-  state = { artistQuery: "", artist: null, tracks: [] };
+  state = { artist: null, tracks: [] };
 
   updateArtistQuery = () => {
     this.setState({ artistQuery: event.target.value });
@@ -17,8 +18,8 @@ class App extends Component {
     }
   };
 
-  searchArtist = () => {
-    fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+  searchArtist = artistQuery => {
+    fetch(`${API_ADDRESS}/artist/${artistQuery}`)
       .then(response => response.json())
       .then(json => {
         if (json.artists.total > 0) {
@@ -40,12 +41,7 @@ class App extends Component {
     return (
       <div>
         <h2>Music Master</h2>
-        <input
-          onChange={this.updateArtistQuery}
-          onKeyPress={this.handleKeyPress}
-          placeholder="Search for an Artist"
-        />
-        <button onClick={this.searchArtist}>Search</button>
+        <Search searchArtist={this.searchArtist} />
         <Artist artist={this.state.artist} />
         <Tracks tracks={this.state.tracks} />
       </div>
