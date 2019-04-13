a60d69938df2242ca7b47cd56223213b6b6751b7
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-04-13 13:16:23 -0400

    Added axios get method to return results.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,7 +3,16 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {}
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 78ad77397a8f30eb27f4a92621de18765262e691d2451ad12fbe80ffdb7ac74c"
+      }
+    });
+  }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
